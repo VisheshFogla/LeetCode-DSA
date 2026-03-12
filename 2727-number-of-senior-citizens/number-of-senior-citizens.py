00ca3cbba1ffb3e03aad0da1b187ceeb97e7f89e
VisheshFogla <83183126+VisheshFogla@users.noreply.github.com>
2026-03-12 21:54:43 +0000

    Time: 4 ms (6.08%) | Memory: 19.4 MB (14.53%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        
+        count = 0
+
+        for i in range (0, len(details)):
+            
+            if (int(details[i][11])*10 + int(details[i][12])) > 60:
+                count = count + 1
+        
+        return count
+            
